08733be93d790891c44bcecc72a07c0574058872
lukerlars <lukerlars@gmail.com>
2024-01-28 19:04:07 +0100

    Run rust tests

--- src/main.rs
+++ src/main.rs
@@ -1,129 +1,43 @@
+#![allow(unused_variables)]
+mod sorting_aglorithms;
+use sorting_aglorithms::{heapsort,merge_sort,insertion_sort};
+
+use std::time::Instant;
+use rand::Rng;
+use rand::distributions::Uniform;
 
 
-// ---------  Insertion sort -------------
 
-fn insertion_sort(input : &Vec<i32>) -> Vec<i32>{
-    let mut work_vec  =  input.clone();
-    for j in 1..input.len(){
-        let key = work_vec[j];
-        let mut i : usize  = j;
-        while i > 0 && work_vec[i - 1] > key {
-            work_vec[i] = work_vec[i- 1];
-            i -= 1;
-            work_vec[i] = key;
-        }
-    }
-    work_vec
+fn create_random_integer_vector(n: usize) -> Vec<i32> {
+    let mut rng = rand::thread_rng();
+    let range = Uniform::new_inclusive(0, 100);
+
+    (0..n).map(|_| rng.sample(&range)).collect()
 }
 
-// ------- Merge sort ----------------
-
-fn merge(array: &mut [i32], p: usize,q: usize,r : usize ) {
-    let n1 = q - p ;
-    let n2 = r - q ;
-
-    let mut l_array : Vec<i32> = Vec::with_capacity(n1 +1);
-    let mut r_array : Vec<i32> = Vec::with_capacity(n2 +1);
-
-    for i in 0..n1 {
-        l_array.push(array[p + i]);
-    }
-    for j in 0..n2 {
-        r_array.push(array[q + j]);
-    }
-    
-    let (mut i, mut j, mut k) = (0, 0, p);
-    while i < n1 && j < n2 {
-        if l_array[i] <= r_array[j] {
-            array[k] = l_array[i];
-            i += 1;
-        } else {
-            array[k] = r_array[j];
-            j += 1;
-        }
-        k += 1;
-    }
-    while i < n1 {
-        array[k] = l_array[i];
-        i += 1;
-        k += 1;
-    }
-    while j < n2 {
-        array[k] = r_array[j];
-        j += 1;
-        k += 1;
-    }
-}
-
-fn merge_sort(array : &mut [i32], p : usize, r: usize) {
-    if p < r - 1 {
-        let q = p + (r - p)/ 2; 
-        merge_sort(array, p, q);
-        merge_sort(array, q, r);
-        merge(array, p, q, r);
-    }
-
-}
-
-// ----- Heapsort -----------
-
-fn heapify(array: &mut [i32], idx: usize, heapsize : usize){
-    
-    let left = |idx:usize| 2*idx +1;
-    let right = |idx: usize| 2*idx +2;
-
-    let l: usize = left(idx);
-    let r: usize = right(idx);
-
-    let mut largest = idx;
-
-    if l < heapsize && array[l] > array[idx]{
-        largest = l;
-    }
-    if r < heapsize && array[r] > array[largest]{
-        largest = r;
-    }
-
-    if largest != idx {
-        array.swap(idx, largest);
-        heapify(array, largest, heapsize);
-    }
-
-}
-
-fn max_heap(array: &mut [i32],heapsize : usize ){
-    for i in (0..array.len()/2 ).rev(){
-        heapify(array, i,heapsize);   
-    }
-}
-
-fn heapsort(array: &mut [i32]){
-    let mut heapsize = array.len();
-    max_heap(array, heapsize);
-        for i in (1..array.len()).rev() {
-            array.swap(0,i);
-            heapsize -= 1;
-            max_heap(array, heapsize);
-    }
-}
-
+const TEST_SIZE:usize = 100000;
 
 fn main() {
 
-    // let some_vec: Vec<i32> = vec![7,6,3,1,4,5];
-    // let a = insertion_sort(&some_vec);
-    // println!("Is it sorted? {:?}" , a)
-
-    // let mut vec = vec![3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
-    let mut vec = vec![4, 1, 3, 2, 16, 9, 10, 14, 8, 7];
-    // let length = vec.len();
-    // merge_sort(&mut vec[..], 0, length);
-    heapsort(&mut vec[..]);
-    // max_heap(&mut vec[..]);
+    let insertion_test_vector = create_random_integer_vector(TEST_SIZE);
+    let insertion_start = Instant::now();
+    let insertion_sorted = insertion_sort(&insertion_test_vector);
+    let insertion_duration= insertion_start.elapsed();
+    println!("Insertion sort with a random vector of size {}, took {:?} to sort",TEST_SIZE, insertion_duration);
 
 
 
+    let mut merge_test_vector = create_random_integer_vector(TEST_SIZE);
+    let merge_start = Instant::now();
+    merge_sort(&mut merge_test_vector[..], 0, TEST_SIZE);
+    let merge_duration= merge_start.elapsed();
+    println!("Merge sort with a random vector of size {}, took {:?} to sort",TEST_SIZE, merge_duration);
 
-    println!("{:?}", vec);
+
+    let mut heap_test_vector = create_random_integer_vector(TEST_SIZE);
+    let heap_start = Instant::now();
+    heapsort(&mut heap_test_vector[..]);
+    let heap_duration= heap_start.elapsed();
+    println!("Heap sort with a random vector of size {}, took {:?} to sort",TEST_SIZE, heap_duration);
+
 }
-
---
+++ src/sorting_aglorithms.rs
@@ -0,0 +1,107 @@
+
+// ---------  Insertion sort -------------
+
+pub fn insertion_sort(input : &Vec<i32>) -> Vec<i32>{
+    let mut work_vec  =  input.clone();
+    for j in 1..input.len(){
+        let key = work_vec[j];
+        let mut i : usize  = j;
+        while i > 0 && work_vec[i - 1] > key {
+            work_vec[i] = work_vec[i- 1];
+            i -= 1;
+            work_vec[i] = key;
+        }
+    }
+    work_vec
+}
+
+// ------- Merge sort ----------------
+
+fn merge(array: &mut [i32], p: usize,q: usize,r : usize ) {
+    let n1 = q - p ;
+    let n2 = r - q ;
+
+    let mut l_array : Vec<i32> = Vec::with_capacity(n1 +1);
+    let mut r_array : Vec<i32> = Vec::with_capacity(n2 +1);
+
+    for i in 0..n1 {
+        l_array.push(array[p + i]);
+    }
+    for j in 0..n2 {
+        r_array.push(array[q + j]);
+    }
+    
+    let (mut i, mut j, mut k) = (0, 0, p);
+    while i < n1 && j < n2 {
+        if l_array[i] <= r_array[j] {
+            array[k] = l_array[i];
+            i += 1;
+        } else {
+            array[k] = r_array[j];
+            j += 1;
+        }
+        k += 1;
+    }
+    while i < n1 {
+        array[k] = l_array[i];
+        i += 1;
+        k += 1;
+    }
+    while j < n2 {
+        array[k] = r_array[j];
+        j += 1;
+        k += 1;
+    }
+}
+
+pub fn merge_sort(array : &mut [i32], p : usize, r: usize) {
+    if p < r - 1 {
+        let q = p + (r - p)/ 2; 
+        merge_sort(array, p, q);
+        merge_sort(array, q, r);
+        merge(array, p, q, r);
+    }
+
+}
+
+// ----- Heapsort -----------
+
+fn heapify(array: &mut [i32], idx: usize, heapsize : usize){
+    
+    let left = |idx:usize| 2*idx +1;
+    let right = |idx: usize| 2*idx +2;
+
+    let l: usize = left(idx);
+    let r: usize = right(idx);
+
+    let mut largest = idx;
+
+    if l < heapsize && array[l] > array[idx]{
+        largest = l;
+    }
+    if r < heapsize && array[r] > array[largest]{
+        largest = r;
+    }
+
+    if largest != idx {
+        array.swap(idx, largest);
+        heapify(array, largest, heapsize);
+    }
+
+}
+
+fn max_heap(array: &mut [i32],heapsize : usize ){
+    for i in (0..array.len()/2 ).rev(){
+        heapify(array, i,heapsize);   
+    }
+}
+
+pub fn heapsort(array: &mut [i32]){
+    let mut heapsize = array.len();
+    max_heap(array, heapsize);
+        for i in (1..array.len()).rev() {
+            array.swap(0,i);
+            heapsize -= 1;
+            max_heap(array, heapsize);
+    }
+}
